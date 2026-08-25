eb68f58ce7dcdccbc0fc0e3d3bd0f18bc066cbd0
PavanTej-88 <is.pavantej88@gmail.com>
2026-08-25 11:41:05 +0530

    all project files

---
+++ test2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,s;
+    cout <<"Enter a and b: "<<endl;
+    cin >>a >>b;
+    s=a+b;
+    cout <<a <<"+" <<b <<"=" <<s;
+    return 0;
+}
